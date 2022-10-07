a38c366498fbfd87c390c09c692cc0c75a204270
muradorucov <orucowmurad@gmail.com>
2022-10-07 12:07:22 +0400

    feat: admin dashboard table done

--- src/pages/admin/AdminRoot.jsx
+++ src/pages/admin/AdminRoot.jsx
@@ -1,13 +1,63 @@
-import { Footer, Header } from 'antd/lib/layout/layout'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React from 'react';
+import Dashboard from './Dashboard';
+const { Header, Content, Footer } = Layout;
+
+
 
 function AdminRoot() {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <Layout>
+        <Header
+          style={{
+            position: 'fixed',
+            zIndex: 1,
+            width: '100%',
+          }}
+        >
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={new Array(3).fill(null).map((_, index) => ({
+              key: String(index + 1),
+              label: `nav ${index + 1}`,
+            }))}
+          />
+        </Header>
+        <Content
+          className="site-layout"
+          style={{
+            padding: '0 50px',
+            marginTop: 64,
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Admin</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 380,
+            }}
+          ><Dashboard/>
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     </>
   )
 }

34a0eb0e79d9ddeb726744168eab32234da60ec5
hnuruzada <90691865+hnuruzada@users.noreply.github.com>
2022-10-07 11:52:21 +0400

    payment form added

--- src/pages/companyAdmin/CreateLink/index.jsx
+++ src/pages/companyAdmin/CreateLink/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import FormPayment from "../../../components/comapnyAdmin";
+import "./link.css";
 
 function CreateLink() {
-  return <div>CreateLink</div>;
+  return (
+    <div className="create-link">
+      <div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+            src="https://i.pinimg.com/736x/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg"
+            alt="companylogo"
+          />
+          <h2>Company Name</h2>
+        </div>
+        <div className="description">
+          <p>Description</p>
+          <p className="price">Price</p>
+        </div>
+      </div>
+      <FormPayment />
+    </div>
+  );
 }
 
 export default CreateLink;
